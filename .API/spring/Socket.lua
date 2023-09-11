12bdd3ce5b97fa24a38eadd40c25cd762a1729ef
ZwerOxotnik <zweroxotnik@gmail.com>
2023-09-11 15:29:28 +0300

    Add a comment

--- .API/spring/Socket.lua
+++ .API/spring/Socket.lua
@@ -2,6 +2,8 @@
 ---@diagnostic disable
 
 
+---For LuaMenu, LuaUI only
+---
 ---[Spring's wiki](https://springrts.com/wiki/Lua_Socket), [Spring's GitHub repository](https://github.com/spring/spring/tree/develop/rts/lib/luasocket), [original source](https://github.com/lunarmodules/luasocket/tree/81ebe649f0134a66137d3266fcd2e1b93d9e0ba3)
 ---@class Socket
 
