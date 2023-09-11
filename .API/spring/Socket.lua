e052dc1f36304e1f827d1cca95e24c5d808af122
ZwerOxotnik <zweroxotnik@gmail.com>
2023-09-11 15:26:59 +0300

    Add Socket.lua

---
+++ .API/spring/Socket.lua
@@ -0,0 +1,10 @@
+---@meta
+---@diagnostic disable
+
+
+---[Spring's wiki](https://springrts.com/wiki/Lua_Socket), [Spring's GitHub repository](https://github.com/spring/spring/tree/develop/rts/lib/luasocket), [original source](https://github.com/lunarmodules/luasocket/tree/81ebe649f0134a66137d3266fcd2e1b93d9e0ba3)
+---@class Socket
+
+
+---@type Socket
+socket = socket
